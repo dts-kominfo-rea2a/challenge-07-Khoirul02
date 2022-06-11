45d890f8ba49f699b508a2fabb0f21d7d2a902ac
khoirul02 <hudak006@gmail.com>
2022-06-11 11:43:48 +0700

    submit v1

--- promise.js
+++ promise.js
@@ -1,8 +1,17 @@
 const { promiseTheaterIXX, promiseTheaterVGC } = require("./external.js");
 
 // TODO: Buat fungsi promiseOutput sesuai ketentuan readme
-const promiseOutput = null;
-
+const promiseOutput = async (rate) => {
+  const theaterIXX = await promiseTheaterIXX();
+  const theaterVGC = await promiseTheaterVGC();
+  const films = theaterIXX.concat(theaterVGC);
+  const result = countRate(rate, films)
+  return result;
+}
+const countRate = (rate, films) => {
+  const filterFilm = films.filter((film) => film.hasil === rate);
+  return filterFilm.lenght;
+}
 module.exports = {
   promiseOutput,
 };
